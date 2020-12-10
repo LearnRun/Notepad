7e225967a792c94743db52a6af5e65be28e0a570
Kimsuseong <kimsusung55@gmail.com>
2020-12-10 18:27:38 +0900

    Init code

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native'
+
+const App = () => {
+  return (
+      <SafeAreaView style={styles.container}>
+        <Text>Hello World!!!</Text>
+      </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+
+export default App
